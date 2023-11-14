accb29cf8207e186ea65e2930fedd52525cefc6a
Maureen Chang <76696006+techMedMau@users.noreply.github.com>
2023-11-13 21:26:10 -0800

    implement http server

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/techMedMau/go_RSS_aggregator
 
 go 1.21.4
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
 	"os"
+	"net/http"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -17,5 +20,30 @@ func main() {
 		log.Fatal("PORT is not found in environment")
 	}
 
-	fmt.Println("Port: " , portString)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
