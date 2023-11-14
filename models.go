79a8b2b4619731439e8bc20fbb682d08e7061ad4
Maureen Chang <76696006+techMedMau@users.noreply.github.com>
2023-11-14 12:21:06 -0800

    Implement createFeed & getFeed APIs

--- models.go
+++ models.go
@@ -7,7 +7,7 @@ import (
 	"github.com/techMedMau/go_RSS_aggregator/internal/database"
 )
 
-// format respond
+// format respond for User
 type User struct {
 	ID uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -24,4 +24,33 @@ func databaseUserToUser(dbUser database.User) User {
 		Name: dbUser.Name,
 		ApiKey: dbUser.ApiKey,
 	}
+}
+
+// format respond for Feed
+type Feed struct {
+	ID uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name string `json:"name"`
+	Url string `json:"url"`
+	UserID uuid.UUID `json:"user_id"`
+}
+
+func databaseFeedToFeed(dbFeed database.Feed) Feed {
+	return Feed {
+		ID: dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name: dbFeed.Name,
+		Url: dbFeed.Url,
+		UserID: dbFeed.UserID,
+	}
+}
+
+func databaseFeedsToFeeds(dbFeeds []database.Feed) []Feed {
+	feeds := []Feed{}
+	for _, dbFeed := range dbFeeds {
+		feeds = append(feeds, databaseFeedToFeed(dbFeed))
+	}
+	return feeds
 }
