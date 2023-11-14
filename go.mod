edee960880bc8d01a1d18fa6654ad239cfebd495
Maureen Chang <76696006+techMedMau@users.noreply.github.com>
2023-11-13 18:06:05 -0800

    project setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/techMedMau/go_RSS_aggregator
 
 go 1.21.4
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("HH")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in environment")
+	}
+
+	fmt.Println("Port: " , portString)
 }
