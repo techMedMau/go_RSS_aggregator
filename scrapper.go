81db03406223bcce3212199fa14fa044e7a7adcf
Maureen Chang <76696006+techMedMau@users.noreply.github.com>
2023-11-19 06:21:48 -0800

    Store RSS feeds to db

--- scrapper.go
+++ scrapper.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"sync"
 	"time"
 	
 	"github.com/techMedMau/go_RSS_aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScraping(
@@ -47,7 +49,33 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 	
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Couldn't parse date %v with err %v", item.PubDate, err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), 
+					database.CreatePostParams{
+						ID: uuid.New(),
+						CreatedAt: time.Now().UTC(),
+						UpdatedAt: time.Now().UTC(),
+						Title: item.Title,
+						Description: description,
+						PublishedAt: pubAt, 
+						Url: item.Link,
+						FeedID: feed.ID,
+					})
+		if err != nil {
+			log.Println("Failed to create post:", err)
+		}
+
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
